a46b1ae1758f64c94ec06ad05ddd919ec6fa4dd8
THorvath1 <72626710+THorvath1@users.noreply.github.com>
2021-04-16 22:41:08 -0700

    added last two cart functions

--- database.py
+++ database.py
@@ -12,23 +12,33 @@ def run_query(sql, params = ()):
     db.close()
     return result
 
+def run_total(sq1, params = ()):
+    db = sqlite3.connect('chinook.db')
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    cursor.execute(sq1, params)
+    result = cursor.fetchone() [0]
+    cursor.close()
+    db.close()
+    return id
+
 def run_insert(sql, params):
     #grab data from DB
     db = sqlite3.connect('chinook.db')
     cursor = db.cursor()
     cursor.execute(sql, params)
-    id = cursor.lastrowid
+    result = cursor.lastrowid
     db.commit()
     cursor.close()
     db.close()
-    return id
+    return result
 
 def run_delete_cart(sq1, params):
     db = sqlite3.connect('chinook.db')
     db.row_factory = sqlite3.Row
     cursor = db.cursor()
     cursor.execute(sq1, params)
-    id = cursor.lastrowid
+    result = cursor.lastrowid
     db.commit()
     cursor.close()
     db.close()
@@ -39,7 +49,18 @@ def run_remove_cart(sq1, params):
     db.row_factory = sqlite3.Row
     cursor = db.cursor()
     cursor.execute(sq1, params)
-    id = cursor.lastrowid
+    result = cursor.lastrowid
+    db.commit()
+    cursor.close()
+    db.close()
+    return result
+
+def run_clear_cart(sq1, params):
+    db = sqlite3.connect('chinook.db')
+    db.row_factory = sqlite3.Row
+    crusor = db.cursor()
+    cursor.execute(sq1)
+    result = cursor.lastrowid
     db.commit()
     cursor.close()
     db.close()
